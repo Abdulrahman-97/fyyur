4dab73ad92f08e7ffa8cbb0d57b85c3000bcc85b
Abdulrahman <aalsoheim@gmail.com>
2021-03-20 20:40:20 +0300

    Implement search_artists()

--- app.py
+++ app.py
@@ -295,16 +295,22 @@ def artists():
 
 @app.route('/artists/search', methods=['POST'])
 def search_artists():
-  # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
-  # seach for "A" should return "Guns N Petals", "Matt Quevado", and "The Wild Sax Band".
-  # search for "band" should return "The Wild Sax Band".
+  current_date =  datetime.now().strftime('%Y-%m-%d %H:%S:%M') # get current time
+  # get search term provided by user
+  search_term = request.form.get('search_term', '')
+  # query artists that include the search_term using ilike which supports case-insensitive filtering
+  artists = db.session.query(Artist.id, Artist.name).filter(Artist.name.ilike(f'%{search_term}%')).all()
+  data = []
+  for artist in artists:
+    num_upcoming_shows = db.session.query(Artist.id, Show).filter(Artist.id==artist[0], Show.start_time>current_date).join(Show, Artist.id == Show.artist_id).count()
+    data.append({
+      "id": artist[0],
+      "name": artist[1],
+      "num_upcoming_shows": num_upcoming_shows,
+    })
   response={
-    "count": 1,
-    "data": [{
-      "id": 4,
-      "name": "Guns N Petals",
-      "num_upcoming_shows": 0,
-    }]
+    "count": len(artists),
+    "data": data
   }
   return render_template('pages/search_artists.html', results=response, search_term=request.form.get('search_term', ''))
 
