4d6877591bdf5f3170c7c50a609351ee9c5cfdd6
Abdulrahman <aalsoheim@gmail.com>
2021-03-24 00:33:53 +0300

    fix venue deletion when on relationship

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ class Venue(db.Model):
     website = db.Column(db.String(120))
     seeking_talent = db.Column(db.Boolean, default=False)
     seeking_desc = db.Column(db.String(500))
-    shows = db.relationship('Show', backref='venue', lazy=True)
+    shows = db.relationship('Show', backref='venue', cascade="all, delete-orphan" , lazy=True)
 
     
     def get_past_shows(self):
