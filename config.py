b7a31aec8f236fda021b59fe37cfb63e232ac451
Abdulrahman <aalsoheim@gmail.com>
2021-03-27 19:10:06 +0300

    Implement environment variables

--- config.py
+++ config.py
@@ -1,13 +1,16 @@
 import os
+from decouple import config
 SECRET_KEY = os.urandom(32)
+USERNAME = config('USER')
+KEY = config('KEY')
 # Grabs the folder where the script runs.
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 # Enable debug mode.
 DEBUG = True
 
-# Connect to the database
-
 
 # TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:123456@127.0.0.1:5432/fyyur'
+SQLALCHEMY_DATABASE_URI = f'postgresql://{USERNAME}:{KEY}@127.0.0.1:5432/fyyur'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+
